13e354815c4bdb02e75701e00197045968e6cf74
2tank <adrianfeve7@gmail.com>
2024-03-20 18:38:30 +0100

    DoubleLinkedTest terminado junto con correciones de la clase DoubleLList

--- MPSP3/src/main/java/mps/Main.java
+++ MPSP3/src/main/java/mps/Main.java
@@ -4,9 +4,18 @@ public class Main {
     public static void main(String[] args) {
 
         DoubleLinkedList<Integer> lista  =new DoubleLinkedList();
-        lista.append(12);
         lista.prepend(11);
-
+        lista.prepend(13);
+        lista.prepend(14);
+        lista.prepend(15);
+        lista.prepend(1);
+        lista.prepend(17);
+        System.out.println(lista.first());
+        System.out.println(lista.last());
+        lista.deleteFirst();
+        lista.deleteLast();
+        System.out.println(lista.first());
+        System.out.println(lista.last());
 
     }
 }
